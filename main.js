b76195f8890bfa929a8bc4fc37fe061b82865fab
swus1978 <juang4u@gmail.com>
2023-06-07 17:41:44 -0500

    updated the array of cart items

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // this is an empty array and it will be filling up with items ...
-const cart = [{ image: '🥦', codes: '00111', type: 'pineapple cush', price: 50 },];
+const cart = [{ image: '🥦', codes: '00111', type: 'veggies a la cart', price: 50 },];
 // These are the my items to be sold.
 const items = [
-    { image: '🎄', codes: '00666', type: 'rag weed cush', price: 100 },
-    { image: '🌳', codes: '00211', type: 'hood cush', price: 50 },
-    { image: '🍀', codes: '00420', type: 'mango weed cush', price: 100 },
+    { image: '🎄', codes: '00666', type: 'grilled veggies', price: 100 },
+    { image: '🌳', codes: '00211', type: 'stir fry veggies', price: 50 },
+    { image: '🍀', codes: '00420', type: 'steam veggies', price: 100 },
 ];
 
 
